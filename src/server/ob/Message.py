b4850bd814ee1fee2f341785e5186bf105320d92
FabianGote <80948098+FabianGote@users.noreply.github.com>
2021-05-02 16:53:14 +0200

    Message

---
+++ src/server/ob/Message.py
@@ -0,0 +1,10 @@
+class Message():
+
+    def __init__(self):
+        self._id = 0
+
+    def get_Content(self):
+        return self._Content
+
+    def set_Content(self):
+        self._Content = value
