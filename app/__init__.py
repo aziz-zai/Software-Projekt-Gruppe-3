8a1b88c93dd7adff685be1135783e90553ce2447
aziz-zai <aziz.z@gmx.de>
2021-05-23 17:14:34 +0200

    namespace routes hinzugefügt

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 from app.apps.profile.ProfileViews import namespace as profile_namespace
 from app.apps.person.PersonViews import namespace as person_namespace
 from app.apps.message.MessageViews import namespace as message_namespace
+from app.apps.conversation.ConversationViews import namespace as conversation_namespace
+from app.apps.group.GroupViews import namespace as group_namespace
 
 
 load_dotenv()
@@ -35,6 +37,8 @@ def init_routes() -> None:
     api.add_namespace(profile_namespace)
     api.add_namespace(person_namespace)
     api.add_namespace(message_namespace)
+    api.add_namespace(conversation_namespace)
+    api.add_namespace(group_namespace)
 
 if __name__ == "__main__":
     app = create_app()
