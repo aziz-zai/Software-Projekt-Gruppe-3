4029defeaeebd0ade23bebebb6540781c71cce16
aziz-zai <am202@hdm-stuttgart.de>
2021-06-03 15:53:15 +0200

    post Profile gelöscht

--- app/apps/profile/ProfileViews.py
+++ app/apps/profile/ProfileViews.py
@@ -21,12 +21,3 @@ class ProfileAPI(Resource):
         profile = ProfileAdministration.get_profile_of_person(pers)
         return profile
 
-
-
-    @api.marshal_with(profile_marshalling, code=201)
-    @api.expect(profile_marshalling)
-    def post(self) -> dict:
-        """Create Profile Endpoint."""
-        profile = ProfileObject(**api.payload)
-        profile = ProfileAdministration.insert_profile(profile=profile)
-        return profile
