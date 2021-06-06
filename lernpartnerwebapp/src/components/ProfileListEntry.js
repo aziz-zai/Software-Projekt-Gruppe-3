97f67ee4c82fcbe096d38b9380046d182a3db213
Zadranm <80890668+Zadranm@users.noreply.github.com>
2021-06-06 19:44:28 +0200

    ProfileListEntry updated

--- lernpartnerwebapp/src/components/ProfileListEntry.js
+++ lernpartnerwebapp/src/components/ProfileListEntry.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Button, ListItem, ListItemSecondaryAction, Link, Typography } from '@material-ui/core';
+import { withStyles, Button, ListItem, ListItemSecondaryAction, Link, Typography, ButtonGroup } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import SwapHoriz from '@material-ui/icons/SwapHoriz';
 import { Link as RouterLink } from 'react-router-dom';
 import { AppAPI } from '../api';
 import ContextErrorMessage from './dialogs/ContextErrorMessage';
 import LoadingProgress from './dialogs/LoadingProgress';
-import MoneyTransferDialog from './dialogs/MoneyTransferDialog';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ProfileForm from './dialogs/ProfileForm'
+import ProfileBO from '../api/ProfileBO'
 class ProfileListEntry extends Component {
 
   constructor(props) {
@@ -15,62 +17,59 @@ class ProfileListEntry extends Component {
 
     // Init an empty state
     this.state = {
-      balance: '',
-      loadingInProgress: false,
-      deletingInProgress: false,
+      profile: new ProfileBO(),
+      showProfileForm: false,
       loadingError: null,
       deletingError: null,
-      showMoneyTransferDialog: false,
     };
   }
 
-    // set loading to true
+
+  updateProfileButton = (event) => {
+    event.stopPropagation();
     this.setState({
-      balance: 'loading',
-      loadingInProgress: true,
-      loadingError: null
+      showProfileForm: true
     });
   }
 
+  profileFormClosed = (profile) => {
+    if (profile) {
+      this.setState({
+        profile: profile,
+        showProfileForm: false
+      });
+    } else {
+      this.setState({
+        showProfileForm: false
+      })
+    }
+  }
 
   /** Renders the component */
   render() {
-    const { classes, customer, account } = this.props;
-    const { loadingInProgress, deletingInProgress, loadingError, deletingError, balance, showMoneyTransferDialog } = this.state;
+    const { classes, profile } = this.props;
+    const { person, showProfileForm, loadingError, loadingInProgress } = this.state;
 
     return (
       <div>
         <ListItem>
-          <Typography className={classes.accountEntry}>
-            <Link component={RouterLink} to={{
-              pathname: '/transactions',
-              owner: {
-                customer: customer,
-                account: account
-              }
-            }} >
-              Account ID: {account.getID()}
-            </Link>
-
+          <Typography className={classes.profileEntry}>
           </Typography>
-          <Typography color='textSecondary'>
-            Balance: {!isNaN(balance) ? BankAPI.getAPI().getCurrencyFormatter().format(balance) : balance}
-          </Typography>
-          <ListItemSecondaryAction>
-            <Button className={classes.buttonMargin} variant='outlined' color='primary' size='small' startIcon={<SwapHoriz />} onClick={this.transferMoney}>
-              Transfer
-            </Button>
-            <Button color='secondary' size='small' startIcon={<DeleteIcon />} onClick={this.deleteAccount}>
-              Delete
-            </Button>
-          </ListItemSecondaryAction>
+                Firstname:      {profile.getFirstName()}, <br></br>
+                Lastname:       {profile.getLastName()}, <br></br>
+                Interests:      {profile.getInterests()}, <br></br>
+                Type:           {profile.getType()}, <br></br>
+                Online:         {profile.getOnline()}, <br></br>
+                Frequency:      {profile.getFrequency()}, <br></br>
+                Expertise:      {profile.getExpertise()}, <br></br>
+                Extroversion:   {profile.getExtroversion()}, <br></br>
+                <ButtonGroup variant='text' size='small'>
+                  <Button color='primary' onClick={this.updateProfileButton}>
+                    Click for edit
+                  </Button>
+                </ButtonGroup>
+          <ProfileForm show={showProfileForm} profile={profile} onClose={this.profileFormClosed} />
         </ListItem>
-        <ListItem>
-          <LoadingProgress show={loadingInProgress || deletingInProgress} />
-          <ContextErrorMessage error={loadingError} contextErrorMsg={`The balance of account ${account.getID()} could not be loaded.`} onReload={this.getBalance} />
-          <ContextErrorMessage error={deletingError} contextErrorMsg={`The account ${account.getID()} could not be deleted.`} onReload={this.deleteAccount} />
-        </ListItem>
-        <MoneyTransferDialog show={showMoneyTransferDialog} customer={customer} account={account} onClose={this.moneyTransferDialogClosed} />
       </div>
     );
   }
@@ -80,33 +79,13 @@ class ProfileListEntry extends Component {
 const styles = theme => ({
   root: {
     width: '100%'
-  }, 
-  buttonMargin: {
-    marginRight: theme.spacing(2),
   },
-  accountEntry: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0,
-  }
 });
 
-/** PropTypes */
-AccountListEntry.propTypes = {
-  /** @ignore */
+ProfileListEntry.propTypes = {
+
   classes: PropTypes.object.isRequired,
-  /** The CustomerBO of this AccountListEntry */
-  customer: PropTypes.object.isRequired,
-  /** The AccountBO to be rendered */
-  account: PropTypes.object.isRequired,
-  /**  
-   * Event Handler function which is called after a sucessfull delete of this account. 
-   * 
-   * Signature: onAccountDeleted(AccountBO account); 
-   */
-  onAccountDeleted: PropTypes.func.isRequired,
-  /** If true, balance is (re)loaded */
-  show: PropTypes.bool.isRequired
+  profile: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(AccountListEntry);
+export default withStyles(styles)(ProfileListEntry);
