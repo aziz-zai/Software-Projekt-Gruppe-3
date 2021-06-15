93fcb24d6ffd97d587aff36c5f70a70060337afb
CemArdic <ca051@hdm-stuttgart.de>
2021-06-15 12:17:47 +0200

    ProfilePerson Matchmaking

--- app/apps/profile/ProfileAdministration.py
+++ app/apps/profile/ProfileAdministration.py
@@ -43,3 +43,42 @@ class Matchmaking:
         with db_connector as db:
             cnx = db._cnx
             return ProfileMapper.find_by_personID(cnx=cnx, person = person.id_)
+
+           
+
+from .profile.ProfileMapper import ProfileMapper
+from app.configs.base import db_connector
+from app.apps.profile.ProfileBO import ProfileObject
+
+@staticmethod
+def matching(person):
+
+    with db_connector as db:
+        cnx = db._cnx
+        myProfile = ProfileMapper.find_by_personID(cnx= cnx, person = person)    
+        profileList = ProfileMapper.find_all(cnx= cnx, person=person)
+
+    result = []         
+    personList = []     
+    groupList = []
+
+    for profile in profileList:
+        profile = ProfileObject
+        value = 0  
+
+        if profile.interests == myProfile.interests:
+            value += 1
+        if profile.type_ == myProfile.type_:
+            value += 1
+        if profile.online == myProfile.online:
+            value += 1
+        if profile.frequency == myProfile.frequency:
+            value += 1
+        if profile.expertise == myProfile.expertise:
+            value += 1
+        if profile.extroversion == myProfile.extroversion:
+            value += 1
+        else: 
+            value +=0
+        if value >= 3:
+            result.append(profile)
