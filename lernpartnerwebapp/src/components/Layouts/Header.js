b7c2da8039f34ea0e3526421441a46458c88876c
aziz-zai <am202@hdm-stuttgart.de>
2021-06-16 14:53:35 +0200

    ajust landingpage after logg in

--- lernpartnerwebapp/src/components/Layouts/Header.js
+++ lernpartnerwebapp/src/components/Layouts/Header.js
@@ -44,9 +44,9 @@ class Header extends Component {
         {
           user ?
             <Tabs indicatorColor='primary' textColor='primary' centered value={this.state.tabindex} onChange={this.handleTabChange} >
-            <Tab label='My Profile' component={RouterLink} to={`/customers`} />
-            <Tab label='find Learning partner' component={RouterLink} to={`/customer`} />
             <Tab label='All Profiles' component={RouterLink} to={`/accounts`} />
+            <Tab label='find Learning partner' component={RouterLink} to={`/customer`} />
+            <Tab label='My Profile' component={RouterLink} to={`/customers`} />
             <Tab label='About' component={RouterLink} to={`/about`} />
             </Tabs>
             : 
