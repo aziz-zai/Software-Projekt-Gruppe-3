233ebda15cf801603de48e11e66b85227964502c
aziz-zai <am202@hdm-stuttgart.de>
2021-06-30 21:40:27 +0200

    fixed matchmaking show

--- lernpartnerwebapp/src/components/Matchmaking.js
+++ lernpartnerwebapp/src/components/Matchmaking.js
@@ -15,8 +15,8 @@ class Matchmaking extends Component {
 
     // Init the state
     this.state = {
-      personList: [],
-      groupList: [],
+      personList: null,
+      groupList: null,
     };
   }
   componentDidMount() {
@@ -38,7 +38,6 @@ class Matchmaking extends Component {
       )
       .catch((e) =>
         this.setState({
-          profile: null,
           loadingInProgress: false,
           loadingError: e,
         })
@@ -62,7 +61,6 @@ class Matchmaking extends Component {
       )
      .catch((e) =>
         this.setState({
-          groupList: [],
           loadingInProgress: false,
           loadingError: e,
         })
@@ -84,26 +82,23 @@ class Matchmaking extends Component {
 
     return (
       <div>
-        {personList ? (
+        {(personList?.length) ? (
             <div>
               <Typography className={classes.root} variant='h5' align='center'> Matched Persons:</Typography>
               {
             personList.map(person => 
             <ProfileDetail personList={true} key={person.id_} person={person.id_} //expandedState={expandedProfileID === profile.getID()}
             />)
-
-              }
-
-            {groupList ?
+              }</div>
+        ) : null
+          }
+            {(groupList?.lenght) ?
             <div>
             <Typography className={classes.root} variant='h5' align='center'>Matched Groups:</Typography>
             {groupList.map(group =>
             <GroupDetail showRequestGroup={true} key={group.id_} learngroup={group} //expandedState={expandedProfileID === profile.getID()}
             />)}</div>
               :null}
-            </div>
-        ) : null
-          }
       </div>
     );
   }
